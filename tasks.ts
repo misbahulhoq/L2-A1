123bce4ef506baf8692d7f161288c89769474f5c
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-05-07 22:09:51 +0600

    problem 7 solved

---
+++ tasks.ts
@@ -0,0 +1,13 @@
+enum Day {
+  Monday = "Weekday",
+  Tuesday = "Weekday",
+  Wednesday = "Weekday",
+  Thursday = "Weekday",
+  Friday = "Weekday",
+  Saturday = "Weekend",
+  Sunday = "Weekend",
+}
+
+function getDayType(day: Day): string {
+  return day;
+}
