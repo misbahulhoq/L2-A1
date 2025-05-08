a6f54ec8b1e51d40d5292e91a3853ffbeb8e5852
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-05-08 18:28:36 +0600

    fixed all the problems

--- tasks.ts
+++ tasks.ts
@@ -84,10 +84,19 @@ const products = [
 // Output: { name: "Bag", price: 50 }
 
 async function squareAsync(n: number): Promise<number> {
-  if (n < 0) {
-    throw new Error("Negative numbers are not allowed");
-  }
+  return new Promise((resolve, reject) => {
+    if (n >= 0) {
+      setTimeout(() => {
+        resolve(n * n);
+      }, 1000);
+    } else {
+      reject(new Error("Negative number not allowed"));
+    }
+  });
 }
+
+squareAsync(4).then(console.log); // Output after 1s: 16
+//squareAsync(-3).catch(console.error); // Output: Error: Negative number not allowed
 
 enum Day {
   Monday = "Weekday",
