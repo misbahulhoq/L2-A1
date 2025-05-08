02eea6a9a7ec34a6e195320c1693900907c85b0b
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-05-08 18:22:36 +0600

    solving problem 7

--- tasks.ts
+++ tasks.ts
@@ -28,6 +28,67 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 }
 // console.log(concatenateArrays([1, 2], [3, 4], [5]));
 
+class Vehicle {
+  private _make: string;
+  private _year: number;
+  constructor(make: string, year: number) {
+    this._make = make;
+    this._year = year;
+  }
+  getInfo() {
+    return `Make: ${this._make}, Year ${this._year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private _model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this._model = model;
+  }
+
+  getModel() {
+    return `Model: ${this._model}`;
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+//console.log(myCar.getInfo()); // Output: "Make: Toyota, Year: 2020"
+//console.log(myCar.getModel()); // Output: "Model: Corolla"
+
+function processValue(value: string | number): number {
+  if (typeof value === "string") return value.length;
+  return value * 2;
+}
+//console.log(processValue("hello")); // Output: 5
+//console.log(processValue(10)); // Output: 20
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) return null;
+  products.sort((a, b) => a.price - b.price);
+  return products[products.length - 1];
+}
+
+const products = [
+  { name: "Pen", price: 10 },
+  { name: "Notebook", price: 25 },
+  { name: "Bag", price: 50 },
+];
+
+// console.log(getMostExpensiveProduct(products));
+// Output: { name: "Bag", price: 50 }
+
+async function squareAsync(n: number): Promise<number> {
+  if (n < 0) {
+    throw new Error("Negative numbers are not allowed");
+  }
+}
+
 enum Day {
   Monday = "Weekday",
   Tuesday = "Weekday",
